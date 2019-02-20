ccce357125bb47e0019ddef338001873d1731e5d
PansevichVitali <egyptiain94@gmail.com>
2019-02-20 22:56:04 +0300

    implementation task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const objectExchange = {"H":50, "Q":25, "D":10, "N":5,"P":1}
+    if(currency > 10000)
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if(currency === 0) return {};
+    for(let key in objectExchange){
+        let col = 0;
+        if (currency >= objectExchange[key]){
+            while(currency >= objectExchange[key]){
+                currency -= objectExchange[key]
+                col++
+            }
+            objectExchange[key] = col;
+        }
+        else delete objectExchange[key];
+    }
+    return objectExchange;
 }
