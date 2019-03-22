4af53413cccbbe3f6e4e869278453af61321090f
PansevichVitali <egyptiain94@gmail.com>
2019-03-23 01:36:52 +0300

    style: change white-space

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 module.exports = function makeExchange(currency) {
-    const objectExchange = {"H":50, "Q":25, "D":10, "N":5,"P":1}
-    if(currency > 10000)
-        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
-    if(currency === 0) return {};
-    for(let key in objectExchange){
-        let col = 0;
-        if (currency >= objectExchange[key]){
-            while(currency >= objectExchange[key]){
-                currency -= objectExchange[key]
-                col++
-            }
-            objectExchange[key] = col;
-        }
-        else delete objectExchange[key];
+  const objectExchange = {"H":50, "Q":25, "D":10, "N":5,"P":1}
+  if (currency > 10000)
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  if (currency === 0) return {};
+  for (let key in objectExchange) {
+    let col = 0;
+    if (currency >= objectExchange[key]) {
+      while (currency >= objectExchange[key]) {
+        currency -= objectExchange[key];
+        col++;
+      }
+      objectExchange[key] = col;
     }
-    return objectExchange;
+    else delete objectExchange[key];
+  }
+  return objectExchange;
 }
